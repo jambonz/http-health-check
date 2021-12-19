c0dce25c514a65978929576477d0abcfb1f7fd8e
Dave Horton <daveh@beachdognet.com>
2021-12-19 10:52:30 -0500

    added action to publish

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const assert = require('assert');
 
-module.exports = ({port, logger, path = '/', fn}) => {
-  const app = express();
-  app.use(express.urlencoded({ extended: true }));
-  app.use(express.json());
-  app.get(path, (req, res) => {
+module.exports = ({port, logger, path = '/', app:existingApp, fn}) => {
+  assert.ok(typeof fn === 'function', 'http-health-check: \'fn\' function not suuplied');
+  const app = existingApp || express();
+  if (!existingApp) {
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
+  }
+  app.get(path, async(req, res) => {
     try {
-      const count = fn();
+      const count = await fn();
       if (count >= 0) return res.status(200).json({calls: count});
     } catch (err) {
       logger && logger.info({err}, 'health-checker fn threw error');
@@ -15,7 +19,7 @@ module.exports = ({port, logger, path = '/', fn}) => {
   });
 
   try {
-    app.listen(port, () => {
+    if (!existingApp) app.listen(port, () => {
       logger && logger.info(`health-checker listening on ${port}`);
     });
   } catch (err) {
